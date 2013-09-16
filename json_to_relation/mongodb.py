baa491cb0f13a7d2f4aee803658acc435e4041c6
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-09-16 15:39:33 -0700

    Finished MongoDB wrapper

--- json_to_relation/mongodb.py
+++ json_to_relation/mongodb.py
@@ -4,8 +4,29 @@ from pymongo import MongoClient
 
 
 class MongoDB(object):
+    '''
+    Very simple Python interface to MongoDB. Based on pymongo,
+    this class provides methods to get and set default databases
+    and collections, to insert documents, query collections, and
+    clear all documents in a collection. 
+    
+    The query() method encapsulates MangoDB native methods find() 
+    and find_one(). The query() method makes it very convenient to
+    to request only particular sets of fields (columns in relational 
+    terms). Example:
+        myMongoDb.query({'lname' : 'Doe'}, ('fname', 'lname', 'age'))
+    '''
+    
+    # ----------------------------  Public Methods -------------------
     
     def __init__(self, host="localhost", port=27017):
+        '''
+        Create a connection to the MongoDB demon on the given host/port.
+        @param host: host name where MongoDB demon is running. Can be IP address as string.
+        @type host: String
+        @param port: MongoDB demon's port
+        @type port: int
+        '''
         self.host = host
         self.port = port
         self.client = MongoClient(host, port)
@@ -13,48 +34,156 @@ class MongoDB(object):
         self.set_collection('test_collection')
          
     def set_db(self, dbName):
+        '''
+        Establish a default database within MongoDB for subsequent calls 
+        to other methods of this class.
+        @param dbName: MongoDB database name
+        @type dbName: String
+        '''
         self.dbName = dbName
         self.db = self.client[dbName]
         
-    def get_db(self):
-        return self.db
-    
     def get_db_name(self):
+        '''
+        Obtain the name of the MongoDB database that is
+        currently the default for calls to methods of this class.
+        @rtype: String
+        '''
         return self.dbName
 
     def set_collection(self, collName):
+        '''
+        Establish a default MongoDB collection for subsequent calls to 
+        other methods of this class.
+        @param collName: MongoDB collection name
+        @type collName: String
+        '''
         self.coll = self.db[collName]
         
-    def get_collection(self):
-        return self.coll
-    
     def get_collection_name(self):
+        '''
+        Obtain the name of the MongoDB collection that is
+        currently the default for calls to methods of this class.
+        @rtype: String
+        '''
         return self.coll.name
         
-    def find(self, mongoQuery, colNameTuple=(), limit=0, db=None, collection=None):
+    def query(self, mongoQuery, colNameTuple=(), limit=0, db=None, collection=None):
+        '''
+        Method for querying the database. The mongoQuery parameter is a 
+        dictionary conforming to the MongoDB query conventions. This query
+        is passed through to the underlying MongoDB.
+        
+        The colNameTuple contains a list of field (a.k.a. relational column) names.
+        Result documents will contain only those fields. In contrast to MangoDB
+        convention, the _id field is not automatically returned in the result dictionaries.
+        This field is only included if the caller lists it in colNameTuple. If colNameTuple
+        is an empty tuple, the entirety of each document is returned for each query result.
+        
+        The limit parameter determines how many documents will be returned. A value of zero
+        returns the entire result set. A value of 1 makes the method's behavior analogous to
+        MangoDB's native find_one() method.
+        
+        The db and collection keyword arguments allow callers to address a MongoDB database
+        and/or collection that are not the default. After the method returns, the default
+        database and collection values will still be untouched. 
+        
+        @param mongoQuery: MangoDB query
+        @type mongoQuery: Dict<String,<any>>
+        @param colNameTuple: a possibly empty tuple of field/column names to retrieve for each result document
+        @type colNameTuple: (String)
+        @param limit: maximum number of documents to return
+        @type limit: int
+        @param db: name of MongoDB database other than the current default
+        @type db: String
+        @param collection: name of MongoDB collection other than the current default
+        @type collection: String
+        @rtype: {Dictionary | MangoDB cursor} 
+        '''
         with newMongoDB(self, db) as db, newMongoColl(self, collection) as coll:
             # Turn the list of column names to return into
-            # Mongo-speak:
+            # Mongo-speak. First, take care of the Python weirdness
+            # that turn single-element tuples into the element
+            # themselve: ("foo", "bar") ---> ("foo", "bar"). BUT
+            #            ("foo") ---> "foo". Whereas
+            #            ("foo,") ---> ("foo",). Whereas
+            if not isinstance(colNameTuple, tuple):
+                colNameTuple = (str(colNameTuple),)
+                  
+            # Create dict {"colName1" : 1, "colName2" : 1,...} 
             colsToReturn = {}
             for colName in colNameTuple:
                 colsToReturn[colName] = 1
-            if limit == 1:            
-                return coll.find_one(mongoQuery, colsToReturn)
+            # MongoDB insists on returning the '_id' field, even
+            # if you don't ask for it. Suppress that behavior
+            # iff (i) caller did specify at least one col name (i.e.
+            #     didn't just pass in an empty tuple to get all
+            #     columns, but also (ii) did *not* ask for '_id'
+            try:
+                if len(colsToReturn) > 0 and colsToReturn['_id']:
+                    pass
+            except KeyError:
+                # Caller did not explicitly ask for the _id field,
+                # yet did ask for at least *one* col name. So 
+                # suppress _id:
+                colsToReturn['_id'] = 0
+                
+            if limit == 1:
+                if len(colsToReturn) > 0:          
+                    return coll.find_one(mongoQuery, colsToReturn)
+                else:
+                    return coll.find_one(mongoQuery)
             else:
-                return coll.find(mongoQuery, colsToReturn, limit=limit)
+                if len(colsToReturn) > 0:
+                    return coll.find(mongoQuery, colsToReturn, limit=limit)
+                else:
+                    return coll.find(mongoQuery, limit=limit)
     
     def clear_collection(self, db=None, collection=None):
+        '''
+        Remove all documents from a collection. The affected database/collection
+        are the current defaults, if database/collection are None, else the specified
+        database/collection is affected.
+        @param db: Name of MongoDB database, or None
+        @type db: String
+        @param collection: Name of MongoDB collection, or None
+        @type collection: String
+        '''
         with newMongoDB(self, db) as db, newMongoColl(self, collection) as coll:
-            cursor = self.find({}, colNameTuple=("_id"))
-            for res in cursor:
-                print res
+            coll.remove()
+            
+    def insert(self, doc_or_docs, db=None, collection=None):
+        '''
+        Insert the given dictionary into a MongoDB collection.
+        @param doc_or_docs: Dictionary whose entries are the documents
+        @type doc_or_docs: Dict<String,<any>>
+        @param db: Name of MongoDB database, or None
+        @type db: String
+        @param collection: Name of MongoDB collection, or None
+        @type collection: String
+        '''
+        with newMongoDB(self, db) as db, newMongoColl(self, collection) as coll:
+            coll.insert(doc_or_docs)
+
+
+    # ----------------------------  Private Methods -------------------
     
-    def insert(self, doc_or_docs, pwd=None):
-        if pwd != "unit_testing":
-            raise NotImplementedError("This MongoDB interface is read-only.")
-        self.coll.insert(doc_or_docs)
-        
+    def get_db(self):
+        '''
+        Obtain current default MongoDB database object
+        '''
+        return self.db
+    
+    def get_collection(self):
+        return self.coll
+    
+    # ----------------------------  Private Classes -------------------    
+    
 class newMongoDB:
+    '''
+    Class that enables the construct "with newMongoDB('myDB') as db:"
+    See http://effbot.org/zone/python-with-statement.htm for explanation 
+    '''
     
     def __init__(self, mongoObj, newDbName):
         self.mongoObj = mongoObj
@@ -73,6 +202,10 @@ class newMongoDB:
     
     
 class newMongoColl:
+    '''
+    Class that enables the construct "with newMongoColl('myColl') as coll:"
+    See http://effbot.org/zone/python-with-statement.htm for explanation 
+    '''
     
     def __init__(self, mongoObj, newCollName):
         self.mongoObj = mongoObj
--- json_to_relation/test/test_mongodb.py
+++ json_to_relation/test/test_mongodb.py
@@ -7,7 +7,7 @@ from json_to_relation.mongodb import MongoDB
 import mongomock
 import unittest
 
-TEST_ALL = False
+TEST_ALL = True
 
 class MongoTest(unittest.TestCase):
     '''
@@ -17,52 +17,75 @@ class MongoTest(unittest.TestCase):
 
     def setUp(self):
         # Clear out the mock MongoDB:
-        self.collection = mongomock.Connection().db.testCollection
+        self.collection = mongomock.Connection().db.test_collection
         self.objs = [{"fname" : "Franco", "lname" : "Corelli"}, 
                      {"fname" : "Leonardo", "lname" : "DaVinci", "age" : 300},
                      {"fname" : "Franco", "lname" : "Gandolpho"}]
         
         self.mongodb = MongoDB()
+        self.mongodb.clear_collection(collection="test_collection")
+        self.mongodb.clear_collection(collection="new_coll")
+        self.mongodb.set_collection("test_collection")
 
     @unittest.skipIf(not TEST_ALL, "Skipping")
     def test_update_and_find_one(self):
-        self.mongodb.insert(self.objs[0], "unit_testing")
-        res = self.mongodb.find({"fname" : "Franco"}, 1, collection="test_collection")
+        self.mongodb.insert(self.objs[0])
+        res = self.mongodb.query({"fname" : "Franco"}, limit=1, collection="test_collection")
         self.assertEqual('Corelli', res['lname'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Corelli', res['lname']))
 
     @unittest.skipIf(not TEST_ALL, "Skipping")
     def test_set_coll_use_different_coll(self):
+        # Insert into test_collection:
+        self.mongodb.insert(self.objs[0])
+        # Switch to new_coll:
         self.mongodb.set_collection('new_coll')
-        self.mongodb.insert({"recommendation" : "Hawaii"}, "unit_testing")
+        self.mongodb.insert({"recommendation" : "Hawaii"})
         
         # We're in new_coll; the following should be empty result:
-        res = self.mongodb.find({"fname" : "Franco"}, limit=1)
+        res = self.mongodb.query({"fname" : "Franco"}, limit=1)
         self.assertIsNone(res, "Got non-null result that should be null: %s" % res)
         
         # But this search is within new_coll, and should succeed:
-        res = self.mongodb.find({"recommendation" : {'$regex' : '.*'}}, limit=1)
+        res = self.mongodb.query({"recommendation" : {'$regex' : '.*'}}, limit=1)
         self.assertEqual('Hawaii', res['recommendation'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Hawaii', res['recommendation']))
         
         # Try inline collection switch:
-        res = self.mongodb.find({"fname" : "Franco"}, 1, collection="test_collection")
+        res = self.mongodb.query({"fname" : "Franco"}, limit=1, collection="test_collection")
         self.assertEqual('Corelli', res['lname'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Corelli', res['lname']))
 
         # But the default collection should still be new_coll,
         # so a search with unspecified coll should be in new_coll:
-        res = self.mongodb.find({"recommendation" : {'$regex' : '.*'}}, limit=1)
+        res = self.mongodb.query({"recommendation" : {'$regex' : '.*'}}, limit=1)
         self.assertEqual('Hawaii', res['recommendation'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Hawaii', res['recommendation']))
 
     @unittest.skipIf(not TEST_ALL, "Skipping")
     def test_multi_result(self):
-        resCursor = self.mongodb.find({"fname" : "Franco"})
-        for res in resCursor:
-            print res
-    
+        # Insert two docs with fname == Franco:
+        self.mongodb.insert(self.objs[0])
+        self.mongodb.insert(self.objs[2])
+        resCursor = self.mongodb.query({"fname" : "Franco"})
+        if resCursor.count(with_limit_and_skip=True) != 2:
+            self.fail("Added two Franco objects, but only %d are found." % resCursor.count(with_limit_and_skip=True))
+            
+    @unittest.skipIf(not TEST_ALL, "Skipping")
     def test_clear_collection(self):
+        self.mongodb.insert({"foo" : 10})
+        res = self.mongodb.query({"foo" : 10}, limit=1)
+        self.assertIsNotNone(res, "Did not find document that was just inserted.")
         self.mongodb.clear_collection()
+        res = self.mongodb.query({"foo" : 10}, limit=1)
+        self.assertIsNone(res, "Found document after clearing collection: " + str(res))
         
-        
-                                
+    @unittest.skipIf(not TEST_ALL, "Skipping")
+    def test_only_some_return_columns(self):
+        # Also tests the suppression of _id col when desired:
+        self.mongodb.insert(self.objs[0])
+        self.mongodb.insert(self.objs[1])
+        resCur = self.mongodb.query({}, ("lname"))
+        names = []
+        for lnameDict in resCur:
+            names.append(lnameDict['lname'])
+        self.assertItemsEqual(['Corelli','DaVinci'], names, "Did not receive expected lnames: %s" % str(names))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
