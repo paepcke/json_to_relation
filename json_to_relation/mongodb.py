461ed2dd65bdde14c0813a19b63fe01151498e62
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-09-16 13:51:48 -0700

    Checkpoint: mongodb interface getting better.

--- json_to_relation/mongodb.py
+++ json_to_relation/mongodb.py
@@ -1,6 +1,5 @@
 
 
-import pymongo
 from pymongo import MongoClient
 
 
@@ -9,27 +8,87 @@ class MongoDB(object):
     def __init__(self, host="localhost", port=27017):
         self.host = host
         self.port = port
+        self.client = MongoClient(host, port)
         self.set_db("test")
         self.set_collection('test_collection')
-        self.client = MongoClient(host, port)
          
     def set_db(self, dbName):
+        self.dbName = dbName
         self.db = self.client[dbName]
+        
+    def get_db(self):
+        return self.db
+    
+    def get_db_name(self):
+        return self.dbName
 
     def set_collection(self, collName):
         self.coll = self.db[collName]
         
-    def find(self, mongoQuery, limit=0, db=None, collection=None):
-        if db is None:
-            db = self.db
-        if collection is None:
-            collection = self.coll
-
-        if limit == 1:            
-            return self.client.find_one(mongoQuery)
-        return self.client.find(mongoQuery, limit=limit)
+    def get_collection(self):
+        return self.coll
+    
+    def get_collection_name(self):
+        return self.coll.name
+        
+    def find(self, mongoQuery, colNameTuple=(), limit=0, db=None, collection=None):
+        with newMongoDB(self, db) as db, newMongoColl(self, collection) as coll:
+            # Turn the list of column names to return into
+            # Mongo-speak:
+            colsToReturn = {}
+            for colName in colNameTuple:
+                colsToReturn[colName] = 1
+            if limit == 1:            
+                return coll.find_one(mongoQuery, colsToReturn)
+            else:
+                return coll.find(mongoQuery, colsToReturn, limit=limit)
+    
+    def clear_collection(self, db=None, collection=None):
+        with newMongoDB(self, db) as db, newMongoColl(self, collection) as coll:
+            cursor = self.find({}, colNameTuple=("_id"))
+            for res in cursor:
+                print res
     
     def insert(self, doc_or_docs, pwd=None):
         if pwd != "unit_testing":
             raise NotImplementedError("This MongoDB interface is read-only.")
         self.coll.insert(doc_or_docs)
+        
+class newMongoDB:
+    
+    def __init__(self, mongoObj, newDbName):
+        self.mongoObj = mongoObj
+        self.newDbName = newDbName
+        
+    def __enter__(self):
+        self.savedDBName = self.mongoObj.get_db_name()
+        if self.newDbName is not None:
+            self.mongoObj.set_db(self.newDbName)
+        return self.mongoObj.get_db()
+        
+    def __exit__(self, errType, errValue, errTraceback):
+        self.mongoObj.set_db(self.savedDBName)
+        # Have any exception re-raised:
+        return False
+    
+    
+class newMongoColl:
+    
+    def __init__(self, mongoObj, newCollName):
+        self.mongoObj = mongoObj
+        self.newCollName = newCollName
+    
+    def __enter__(self):
+        self.savedCollName = self.mongoObj.get_collection_name()
+        if self.newCollName is not None:
+            self.mongoObj.set_collection(self.newCollName)
+        return self.mongoObj.get_collection()
+        
+    def __exit__(self, errType, errValue, errTraceback):
+        self.mongoObj.set_collection(self.savedCollName)
+        # Have any exception re-raised:
+        return False
+    
+    
+    
+    
--- json_to_relation/test/test_mongodb.py
+++ json_to_relation/test/test_mongodb.py
@@ -3,8 +3,11 @@ Created on Sep 15, 2013
 
 @author: paepcke
 '''
-import unittest
+from json_to_relation.mongodb import MongoDB
 import mongomock
+import unittest
+
+TEST_ALL = False
 
 class MongoTest(unittest.TestCase):
     '''
@@ -14,12 +17,52 @@ class MongoTest(unittest.TestCase):
 
     def setUp(self):
         # Clear out the mock MongoDB:
-        self.collection = mongomock.Connection().db.collection
+        self.collection = mongomock.Connection().db.testCollection
+        self.objs = [{"fname" : "Franco", "lname" : "Corelli"}, 
+                     {"fname" : "Leonardo", "lname" : "DaVinci", "age" : 300},
+                     {"fname" : "Franco", "lname" : "Gandolpho"}]
         
+        self.mongodb = MongoDB()
 
-    def testFind(self):
-        pass
+    @unittest.skipIf(not TEST_ALL, "Skipping")
+    def test_update_and_find_one(self):
+        self.mongodb.insert(self.objs[0], "unit_testing")
+        res = self.mongodb.find({"fname" : "Franco"}, 1, collection="test_collection")
+        self.assertEqual('Corelli', res['lname'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Corelli', res['lname']))
 
+    @unittest.skipIf(not TEST_ALL, "Skipping")
+    def test_set_coll_use_different_coll(self):
+        self.mongodb.set_collection('new_coll')
+        self.mongodb.insert({"recommendation" : "Hawaii"}, "unit_testing")
+        
+        # We're in new_coll; the following should be empty result:
+        res = self.mongodb.find({"fname" : "Franco"}, limit=1)
+        self.assertIsNone(res, "Got non-null result that should be null: %s" % res)
+        
+        # But this search is within new_coll, and should succeed:
+        res = self.mongodb.find({"recommendation" : {'$regex' : '.*'}}, limit=1)
+        self.assertEqual('Hawaii', res['recommendation'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Hawaii', res['recommendation']))
+        
+        # Try inline collection switch:
+        res = self.mongodb.find({"fname" : "Franco"}, 1, collection="test_collection")
+        self.assertEqual('Corelli', res['lname'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Corelli', res['lname']))
+
+        # But the default collection should still be new_coll,
+        # so a search with unspecified coll should be in new_coll:
+        res = self.mongodb.find({"recommendation" : {'$regex' : '.*'}}, limit=1)
+        self.assertEqual('Hawaii', res['recommendation'], "Failed retrieval of single obj; expected '%s' but got '%s'" % ('Hawaii', res['recommendation']))
+
+    @unittest.skipIf(not TEST_ALL, "Skipping")
+    def test_multi_result(self):
+        resCursor = self.mongodb.find({"fname" : "Franco"})
+        for res in resCursor:
+            print res
+    
+    def test_clear_collection(self):
+        self.mongodb.clear_collection()
+        
+        
+                                
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
