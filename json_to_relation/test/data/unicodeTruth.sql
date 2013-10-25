3dd61b78fc534d0c2a62901b0f23d981c087438b
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-10-25 08:19:40 -0700

    Added unicode test and truth files; forgot during prev commit.

---
+++ json_to_relation/test/data/unicodeTruth.sql
@@ -0,0 +1,120 @@
+USE test;
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS Event, Answer, InputState, CorrectMap, State, Account;
+SET foreign_key_checks = 1;
+CREATE TABLE IF NOT EXISTS Answer (
+    answer_id VARCHAR(32) NOT NULL Primary Key,
+    problem_id TEXT,
+    answer TEXT
+    );
+CREATE TABLE IF NOT EXISTS CorrectMap (
+    correct_map_id VARCHAR(32) NOT NULL Primary Key,
+    answer_id TEXT,
+    correctness TINYTEXT,
+    npoints INT,
+    msg TEXT,
+    hint TEXT,
+    hintmode TINYTEXT,
+    queuestate TEXT
+    );
+CREATE TABLE IF NOT EXISTS InputState (
+    input_state_id VARCHAR(32) NOT NULL Primary Key,
+    problem_id TEXT,
+    state TEXT
+    );
+CREATE TABLE IF NOT EXISTS State (
+    state_id VARCHAR(32) NOT NULL Primary Key,
+    seed TINYINT,
+    done BOOL,
+    problem_id TEXT,
+    student_answer VARCHAR(32),
+    correct_map VARCHAR(32),
+    input_state VARCHAR(32),
+    FOREIGN KEY(student_answer) REFERENCES Answer(answer_id),
+    FOREIGN KEY(correct_map) REFERENCES CorrectMap(correct_map_id),
+    FOREIGN KEY(input_state) REFERENCES InputState(input_state_id)
+    );
+CREATE TABLE IF NOT EXISTS Account (
+    account_id VARCHAR(32) NOT NULL Primary Key,
+    username TEXT,
+    name TEXT,
+    mailing_address TEXT,
+    gender TINYTEXT,
+    year_of_birth TINYINT,
+    level_of_education TINYTEXT,
+    goals TEXT,
+    honor_code BOOL,
+    terms_of_service BOOL,
+    course_id TEXT,
+    enrollment_action TINYTEXT,
+    email TEXT
+    );
+CREATE TABLE IF NOT EXISTS Event (
+    eventID VARCHAR(32),
+    agent TEXT,
+    event_source TINYTEXT,
+    event_type TEXT,
+    ip TINYTEXT,
+    page TEXT,
+    session TEXT,
+    time DATETIME,
+    username TEXT,
+    downtime_for DATETIME,
+    studentID TEXT,
+    instructorID TEXT,
+    courseID TEXT,
+    seqID TEXT,
+    gotoFrom INT,
+    gotoDest INT,
+    problemID TEXT,
+    problemChoice TEXT,
+    questionLocation TEXT,
+    attempts TINYINT,
+    longAnswer TEXT,
+    studentFile TEXT,
+    canUploadFile TINYTEXT,
+    feedback TEXT,
+    feedbackResponseSelected TINYINT,
+    transcriptID TEXT,
+    transcriptCode TINYTEXT,
+    rubricSelection INT,
+    rubricCategory INT,
+    videoID TEXT,
+    videoCode TEXT,
+    videoCurrentTime FLOAT,
+    videoSpeed TINYTEXT,
+    videoOldTime FLOAT,
+    videoNewTime FLOAT,
+    videoSeekType TINYTEXT,
+    bookInteractionType TINYTEXT,
+    success TINYTEXT,
+    answer_id TEXT,
+    hint TEXT,
+    hintmode TINYTEXT,
+    correctness TINYTEXT,
+    msg TEXT,
+    npoints TINYINT,
+    queuestate TEXT,
+    orig_score INT,
+    new_score INT,
+    orig_total INT,
+    new_total INT,
+    event_name TINYTEXT,
+    group_user TINYTEXT,
+    group_action TINYTEXT,
+    position INT,
+    correctMapFK VARCHAR(32),
+    answerFK VARCHAR(32),
+    stateFK VARCHAR(32),
+    accountFK VARCHAR(32),
+    FOREIGN KEY(correctMapFK) REFERENCES CorrectMap(correct_map_id),
+    FOREIGN KEY(answerFK) REFERENCES Answer(answer_id),
+    FOREIGN KEY(stateFK) REFERENCES State(state_id),
+    FOREIGN KEY(accountFK) REFERENCES Account(account_id)
+    );
+START TRANSACTION;
+INSERT INTO Account (account_id,username,name,mailing_address,gender,year_of_birth,level_of_education,goals,honor_code,terms_of_service,course_id,enrollment_action,email) VALUES 
+    ('0fd678e1_dc8b_46c3_a6d8_c24ba5572673','Smith','Randy Smith','Luise-Fischer-Straße 6\0612  80945 Münchhausen','m',0,'m','',0,0,null,null,'rsmith@gmail.com');
+INSERT INTO Event (eventID,agent,event_source,event_type,ip,page,session,time,username,downtime_for,studentID,instructorID,courseID,seqID,gotoFrom,gotoDest,problemID,problemChoice,questionLocation,attempts,longAnswer,studentFile,canUploadFile,feedback,feedbackResponseSelected,transcriptID,transcriptCode,rubricSelection,rubricCategory,videoID,videoCode,videoCurrentTime,videoSpeed,videoOldTime,videoNewTime,videoSeekType,bookInteractionType,success,answer_id,hint,hintmode,correctness,msg,npoints,queuestate,orig_score,new_score,orig_total,new_total,event_name,group_user,group_action,position,correctMapFK,answerFK,stateFK,accountFK) VALUES 
+    ('a0d5d415_bb06_450f_9a6b_cff7ff2cdc40','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML\, like Gecko) Chrome/28.0.1500.72 Safari/537.36','server','/create_account','138.246.171.72',null,null,'2013-08-26T08:24:11.658341+00:00','','0:00:00',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,'0fd678e1_dc8b_46c3_a6d8_c24ba5572673');
+COMMIT;
