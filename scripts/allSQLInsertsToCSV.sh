36670db89b04c26f9bab7043525ebdb4ddc0ef37
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-11-14 11:53:35 -0800

    Made allSQLInsertsToCSV.sh callable from any dir.

--- scripts/allSQLInsertsToCSV.sh
+++ scripts/allSQLInsertsToCSV.sh
@@ -6,12 +6,17 @@
 #
 # When starting and when all done, appends to (hard-coded) directory
 # file sqlInsertsToCSV.log
+#
+# Can be called from anywhere, as long as cd /home/dataman/Data/EdX/tracking/SQL
+# exists.
 
 cd /home/dataman/Data/EdX/tracking/SQL
 # Ensure that log and CSV directories exist:
 mkdir -p ../TransformLogs
 mkdir -p ../CSV
+thisScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "SQLInsertsToCSV start conversion: `date`" >> ../TransformLogs/sqlInsertsToCSV.log
-time parallel --gnu --progress scripts/sqlInsert2CSV.py /home/dataman/Data/EdX/tracking/CSV ::: /home/dataman/Data/EdX/tracking/SQL/*.sql
+time parallel --gnu --progress $thisScriptDir/sqlInsert2CSV.py /home/dataman/Data/EdX/tracking/CSV ::: /home/dataman/Data/EdX/tracking/SQL/*.sql
 echo "SQLInsertsToCSV finished conversion: `date`" >> ../TransformLogs/sqlInsertsToCSV.log
 
