d561212fbf1b879affda1e4cc35d85b4ff0bbd8b
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-12-19 10:56:15 -0800

    Made calls to service scripts of makeScreenNameToAnonTable.sh  relative to location of makeScreenNameToAnonTable.sh.

--- scripts/makeScreenNameToAnonTable.sh
+++ scripts/makeScreenNameToAnonTable.sh
@@ -8,6 +8,9 @@ then
     exit 1
 fi
 
+# Get directory in which this script runs:
+thisScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Given a Grep-compatible search string for finding 
 # course names in zipped OpenEdx tracking logs, and a list of
 # gzipped OpenEdx tracking log file names, produce a CSV table
@@ -19,7 +22,7 @@ tmpScreenNames=mktemp
 # Put a list of screen names from all the .gz 
 # files into a tmp file, one name per line,
 # sorted alphabetically:
-./listCourseScreenNames.sh $@ > $tmpScreenNames
+$thisScriptDir/listCourseScreenNames.sh $@ > $tmpScreenNames
 
 # Pipe the screen names to the script that will 
 # read each screen name from stdin, and output its
@@ -30,7 +33,7 @@ tmpScreenNames=mktemp
 # Remove all-empty lines in the sed command. Finally,
 # replace the tab used to separate the columns
 # screenName and hashValue to a comma:
-cat $tmpScreenNames | ./makeAnonScreenName.py - |  paste $tmpScreenNames - | sed '/^\s*$/d' | sed 's/\t/,/'
+cat $tmpScreenNames | $thisScriptDir/makeAnonScreenName.py - |  paste $tmpScreenNames - | sed '/^\s*$/d' | sed 's/\t/,/'
 
 # Trap ensures that the tmp file is removed even
 # on error:
