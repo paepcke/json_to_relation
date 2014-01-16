549ebaa21c97209ba72a2092db54af8f7e309513
Andreas Paepcke <paepcke@cs.stanford.edu>
2014-01-16 11:38:07 -0800

    Safety checkpoint

--- json_to_relation/cgi_bin/exportClass.py
+++ json_to_relation/cgi_bin/exportClass.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
-
-
 #!/usr/bin/env python
 '''
 Created on Jan 14, 2014
 
 @author: paepcke
 '''
+
 import cgi
 import cgitb
 import os
 import subprocess
 import sys
+import tempfile
 
 
 cgitb.enable()
@@ -19,7 +19,7 @@ cgitb.enable()
 class MockCGI:
     
     def __init__(self):
-        self.parms = {'courseId' : "Engineering/CS106A/Fall2013"}
+        self.parms = {'courseID' : "Engineering/CS106A/Fall2013"}
         
     def getvalue(self, parmName, default=None):
         try:
@@ -36,30 +36,47 @@ class CourseTSVServer:
             self.parms = cgi.FieldStorage()
         # Locate the makeCourseTSV.sh script:
         thisScriptDir = os.path.dirname(__file__)
-        self.exportTSVScript = os.path.join(thisScriptDir, '../../scripts/makeCourseTSVs.sh')
+        self.exportTSVScript = os.path.join(thisScriptDir, '../../scripts/makeCourseCSVs.sh')
+        
+        # A tempfile passed to the makeCourseCSVs.sh script.
+        # That script will place file paths to all created 
+        # tables into that file:
+        self.infoTmpFile = tempfile.NamedTemporaryFile()
     
     def exportClass(self):
-        subprocess.call([self.exportTSVScript, '-u', 'www-data', self.parms.getvalue('courseId', '')],
-                        stdout=sys.stdout, stderr=sys.stderr)
-        
+        theCourseID = self.parms.getvalue('courseID', '')
+        subprocess.call([self.exportTSVScript, '-u', 'www-data', '-i', self.infoTmpFile.name, theCourseID],
+                        stdout=sys.stdout, stderr=sys.stdout)
     
-    def checkClassTables(self):
-        pass
-    
+    def printClassTableInfo(self):
+        for csvFilePath in self.infoTmpFile:
+            sys.stdout.write("Table file %s size: %d" % (csvFilePath, os.path.getsize(csvFilePath)))
+            lineCounter = 0
+            with open(csvFilePath) as infoFd:
+                while lineCounter < 5:
+                    sys.stdout.write(infoFd.readline() + '<br>')
+                    lineCounter += 1
+            sys.stdout.write('<br>')
+            sys.stdout.flush()
+                
     def echoParms(self):
         for parmName in self.parms.keys():
             print("Parm: '%s': '%s'" % (self.parms.getvalue(parmName, '')))
 
 
 if __name__ == '__main__':
-    print("Content-type: text/html\n")
-    
-    print("<html>")
-    print("<head></head>")
-    print("<body>")
+    sys.stdout.write("Content-type: text/html\n\n")
+    sys.stdout.flush()
+
+    sys.stdout.write("<html>")
+    sys.stdout.write("<head></head>")
+    sys.stdout.write("<body>")
+    sys.stdout.flush()
     
     #server = CourseTSVServer(testing=True)
     server = CourseTSVServer(testing=False)
     server.exportClass()
+    server.printClassTableInfo()
 
-    print("</body>")
+    sys.stdout.write("</body>")
+    sys.stdout.flush()
