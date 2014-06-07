9d88170795aef67a877a8219f2566e77c2415287
Andreas Paepcke <paepcke@cs.stanford.edu>
2014-06-07 14:15:59 -0700

    Renamed coursesByQuarter.sh to courseDatesByQuarter.sh. This will change again. This is a safety commit before generalizing this script.

--- scripts/getCourseDatesByQuarter.sh
+++ scripts/getCourseDatesByQuarter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Given a year and the name of a quarter ('fall', 'winter', etc.),
+# write CSV to stdout that contains course name, year, quarter,
+# start date, and end date of the course. One course per line.
+
 USAGE="Usage: coursesByQuarter.sh yyyy {fall | winter | spring | summer}"
 
 if [ $# != 2 ]
@@ -44,6 +48,12 @@ fi
 
 # ----------------------- Create JavaScript --------------
 
+# Create a JavaScript that will be run within a Mongo shell
+# further down. The scripts queries Mongo for all courses
+# within the given quarter's start/end dates. Catching the
+# resulting db cursor, the JavaScript writes to stdout one
+# CSV line after another: <courseName>,<year>,<quarter>,<startDate>,<endDate>
+
 QUERY='courseCursor = db.modulestore.find({"_id.category": "course",
 		           "metadata.start": {$gte: theQuarter, $lt: nextQuarter}},
 	                  {"metadata.start": true, "metadata.end": true}
@@ -52,6 +62,7 @@ QUERY='courseCursor = db.modulestore.find({"_id.category": "course",
 
 
 SCRIPT='var year = '$YEAR';
+        var quarter = '\"$QUARTER\"';
         var fallQuarterStart   = year     + "-09-10T07:59:00Z";
         var winterQuarterStart = (year+1) + "-01-01T07:59:00Z";
         var springQuarterStart = (year+1) + "-03-01T07:59:00Z";
@@ -60,19 +71,28 @@ SCRIPT='var year = '$YEAR';
 	var nextQuarter = '${NEXT_QUARTER}'QuarterStart;
        '$QUERY'
 
+       print("course_display_name,year,quarter,start_date,end_date");
        while (true) {
           doc = courseCursor.hasNext() ? courseCursor.next() : null;
           if (doc == null)
              break;
           print(doc._id.org +
                 "/" + doc._id.course + 
-                "/" + doc._id.name + 
+                "/" + doc._id.name +
+                "," + year +
+                "," + quarter +
                 "," + doc.metadata.start +
                 "," + doc.metadata.end
                 );
        }
 '
 #echo $SCRIPT
+
+# Create a bash command that invokes the Mongo shell
+# with the JavaScript as the command to run:
 cmd="mongo modulestore --quiet --eval '$SCRIPT'"
+
 #echo $cmd
+
+# Run the command:
 eval $cmd
