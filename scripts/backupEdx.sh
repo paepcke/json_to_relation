a37ccd5afcb3535cb5545d4d8d4810c704a2fd26
Andreas Paepcke <paepcke@cs.stanford.edu>
2014-10-11 22:02:28 -0700

    Small correction to deleteIndexForTable.sh. backupEdx.sh still not quite working.

--- scripts/backupEdx.sh
+++ scripts/backupEdx.sh
@@ -8,6 +8,114 @@
 
 # If used outside of Stanford: change the target disk,
 # which at Stanford is /lfs/datastage/1/MySQLBackup/
+#
+# NOTE: will ask for sudo pwd, which limits running by cron
+
+usage="Usage: "`basename $0`" [-u username][-p[pwd]]"
+
+USERNAME=`whoami`
+PASSWD=''
+askForPasswd=false
+
+# -------------------  Process Commandline Option -----------------                
+
+# Check whether given -pPassword, i.e. fused -p with a 
+# pwd string:
+
+for arg in $@
+do
+   # The sed -r option enables extended regex, which
+   # makes the '+' metachar wor. The -n option
+   # says to print only if pattern matches:
+   PASSWD=`echo $arg | sed -r -n 's/-p(.+)/\1/p'`
+   if [ -z $PASSWD ]
+   then
+       continue
+   else
+       #echo "Pwd is:"$PASSWD
+       break
+   fi
+done
+
+
+# Keep track of number of optional args the user provided:
+NEXT_ARG=0
+while getopts ":u:p" opt
+do
+  case $opt in
+    u) # look in given user's HOME/.ssh/ for mysql_root
+      USERNAME=$OPTARG
+      NEXT_ARG=$((NEXT_ARG + 2))
+      ;;
+    p) # ask for mysql root pwd
+      askForPasswd=true
+      NEXT_ARG=$((NEXT_ARG + 1))
+      ;;
+    \?)
+      # If the $PASSWD is set, we *assume* that 
+      # the unrecognized option was a
+      # -pMyPassword, and don't signal
+      # an error. Therefore, if $PASSWD is 
+      # set then illegal options are quietly 
+      # ignored:
+      if [ ! -z $PASSWD ]
+      then 
+	  continue
+      else
+	  echo $USAGE
+	  exit 1
+      fi
+      ;;
+    :)
+      echo $USAGE
+      exit 1
+      ;;
+  esac
+done
+
+# Shift past all the optional parms:
+shift ${NEXT_ARG}
+
+
+if $askForPasswd && [ -z $PASSWD ]
+then
+    # The -s option suppresses echo:
+    read -s -p "Password for $USERNAME on MySQL server: " PASSWD
+    echo
+elif [ -z $PASSWD ]
+then
+    if [ $USERNAME == "root" ]
+    then
+        # Get home directory of whichever user will
+        # log into MySQL:
+	HOME_DIR=$(getent passwd `whoami` | cut -d: -f6)
+        # If the home dir has a readable file called mysql_root in its .ssh
+        # subdir, then pull the pwd from there:
+	if test -f $HOME_DIR/.ssh/mysql_root && test -r $HOME_DIR/.ssh/mysql_root
+	then
+	    PASSWD=`cat $HOME_DIR/.ssh/mysql_root`
+	fi
+    else
+        # Get home directory of whichever user will
+        # log into MySQL:
+	HOME_DIR=$(getent passwd $USERNAME | cut -d: -f6)
+
+        # If the home dir has a readable file called mysql in its .ssh
+        # subdir, then pull the pwd from there:
+	if test -f $HOME_DIR/.ssh/mysql && test -r $HOME_DIR/.ssh/mysql
+	then
+	    PASSWD=`cat $HOME_DIR/.ssh/mysql`
+	fi
+    fi
+fi
+
+# Create the mysql call password option:
+if [ -z $PASSWD ]
+then
+    pwdOption=''
+else
+    pwdOption=$PASSWD
+fi
 
 # Create new directory with name including current date and time:
 # The part `echo \`date\` | sed -e 's/[ ]/_/g'`:
@@ -23,9 +131,13 @@ newDir=/lfs/datastage/1/MySQLBackup/backupEdx_`echo \`date\` | sed -e 's/[ ]/_/g
 sudo mkdir $newDir
 
 # Use mysqlhotcopy to grab one MySQL db at a time:
-sudo mysqlhotcopy Edx $newDir            # ~3hrs
-sudo mysqlhotcopy EdxForum $newDir       # instantaneous
-sudo mysqlhotcopy EdxPiazza $newDir      # instantaneous
-sudo mysqlhotcopy EdxPrivate $newDir     # ~3min
+echo "Backing up Edx db..."
+sudo time mysqlhotcopy --user=$USERNAME --password=$pwdOption Edx $newDir       # ~3hrs
+echo "Backing up EdxForum db..."
+sudo time mysqlhotcopy  --user=$USERNAME --password=$pwdOption EdxForum $newDir       # instantaneous
+echo "Backing up EdxPiazza db..."
+sudo time mysqlhotcopy  --user=$USERNAME --password=$pwdOption EdxPiazza $newDir      # instantaneous
+echo "Backing up EdxPrivate db..."
+sudo time mysqlhotcopy  --user=$USERNAME --password=$pwdOption EdxPrivate $newDir     # ~3min
                                                                                                                          
                                                    
