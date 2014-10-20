2d2203358ca56f6add9bce6b40c0d8be98dae89c
Andreas Paepcke <paepcke@cs.stanford.edu>
2014-10-19 22:23:51 -0700

    Removed trailing comma in partition spec in createEmptyEdxDbs.sql.

--- scripts/createEmptyEdxDbs.sql
+++ scripts/createEmptyEdxDbs.sql
@@ -180,7 +180,7 @@ PARTITION BY LIST COLUMNS(quarter) (
     PARTITION pAY2017_Fall VALUES IN ('fall2017'),
     PARTITION pAY2017_Winter VALUES IN ('winter2018'),
     PARTITION pAY2017_Spring VALUES IN ('spring2018'),
-    PARTITION pAY2017_Summer VALUES IN ('summer2018'),
+    PARTITION pAY2017_Summer VALUES IN ('summer2018')
 ) ENGINE=InnoDB;                            
 
 CREATE TABLE IF NOT EXISTS ActivityGrade (
