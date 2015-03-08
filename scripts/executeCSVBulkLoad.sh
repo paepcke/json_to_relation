52492f0420cd3a5f0aea57951469dc87d549d517
Andreas Paepcke <paepcke@cs.stanford.edu>
2015-03-08 12:03:25 -0700

    In executeCSVBulkLoad.sh, to run on DS2, temporarily disabled load statement.

--- scripts/executeCSVBulkLoad.sh
+++ scripts/executeCSVBulkLoad.sh
@@ -173,7 +173,7 @@ echo "`date`: Done disabling indexing on all tables..."  >> $LOG_FILE 2>&1
 
 # Now load the big file:
 echo "`date`: Start loading from $bulkLoadFile..."  >> $LOG_FILE 2>&1
-mysql $MYSQL_AUTH -f --local_infile=1 < "${bulkLoadFile}" >> "${LOG_FILE}" 2>&1
+#*************mysql $MYSQL_AUTH -f --local_infile=1 < "${bulkLoadFile}" >> "${LOG_FILE}" 2>&1
 echo "`date`: done loading from $bulkLoadFile"  >> $LOG_FILE 2>&1
 
 # Re-build the indexes:
