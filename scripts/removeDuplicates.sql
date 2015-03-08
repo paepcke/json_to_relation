4549fdbd35495b2619e8e7b8ec82cf06cb11de1a
Andreas Paepcke <paepcke@cs.stanford.edu>
2015-03-08 14:32:23 -0700

    In undoDuplicateLoad.sql: added quarter to _id as index columns for EdxTrackEvent. To re-run on DS2: commented tables that were already uniquified.

--- scripts/removeDuplicates.sql
+++ scripts/removeDuplicates.sql
@@ -1,16 +1,22 @@
+# Use to remove all duplicates that were accidentally created
+# when loading the same files multiple times. We do not use 
+# UNIQUE keys to prevent duplicates, because they are prohibitively
+# expensive when loading. In this script we temporarily add
+# a unique key on the one column that is intended to be unique.
+# We remove those indexes again at the end.
+
+# Add UNIQUE index to each of the tables:
+
 USE Edx;
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from ABExperiment...");
-    ALTER IGNORE TABLE `ABExperiment` ADD UNIQUE INDEX (`event_table_id`);
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from ABExperiment...");
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from Answer...");
-    ALTER IGNORE TABLE `Answer` ADD UNIQUE INDEX (`answer_id`);
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from Answer...");
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from CorrectMap...");
-    ALTER IGNORE TABLE `CorrectMap` ADD UNIQUE INDEX (`correct_map_id`);
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from CorrectMap...");
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from EdxTrackEvent...");
-    ALTER IGNORE TABLE `EdxTrackEvent` ADD UNIQUE INDEX (`_id`);
-SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from EdxTrackEvent...");
+-- SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from ABExperiment...");
+--     ALTER IGNORE TABLE `ABExperiment` ADD UNIQUE INDEX (`event_table_id`);
+-- SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from ABExperiment...");
+-- SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from Answer...");
+--     ALTER IGNORE TABLE `Answer` ADD UNIQUE INDEX (`answer_id`);
+-- SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from Answer...");
+-- SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from CorrectMap...");
+--     ALTER IGNORE TABLE `CorrectMap` ADD UNIQUE INDEX (`correct_map_id`);
+-- SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from CorrectMap...");
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from InputState...");
     ALTER IGNORE TABLE `InputState` ADD UNIQUE INDEX (`input_state_id`);
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from InputState...");
@@ -22,6 +28,9 @@ SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from OpenAssessment...");
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from OpenAssessment...");
     ALTER IGNORE TABLE `State` ADD UNIQUE INDEX (`state_id`);
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from State...");
+SELECT CONCAT(CURRENT_TIMESTAMP(), ": Remove dups from EdxTrackEvent...");
+    ALTER IGNORE TABLE `EdxTrackEvent` ADD UNIQUE INDEX (`_id`, `quarter`);
+SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from EdxTrackEvent...");
 
 USE EdxPrivate;
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from Account...");
@@ -31,6 +40,7 @@ SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from EventIp...");
     ALTER IGNORE TABLE `EventIp` ADD UNIQUE INDEX (`event_table_id`);
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done remove dups from EventIp...");
 
+# Remove the UNIQUE indexes from each of the tables:
 
 USE EdX;
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Start deleting the UNIQUE indexes in Edx tables...");
@@ -42,6 +52,7 @@ DROP INDEX input_state_id ON InputState;
 DROP INDEX load_info_id ON LoadInfo;
 DROP INDEX event_table_id ON OpenAssessment;
 DROP INDEX state_id  ON `State`;
+-- ***** DROP INDEX ????  ON `EdxTrackEvent`;
 SELECT CONCAT(CURRENT_TIMESTAMP(), ": Done deleting the UNIQUE indexes in EdxPrivate tables...");
 
 USE EdxPrivate;
