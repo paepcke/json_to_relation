57f438629581e53ce924bd6cddca7014e3bce608
Alex Kindel <atkindel@stanford.edu>
2015-07-30 13:26:30 -0700

    Working on cron script for Qualtrics refresh

--- scripts/cronRefreshEdxQualtrics.py
+++ scripts/cronRefreshEdxQualtrics.py
@@ -9,9 +9,9 @@ import os
 
 # Append directory for dependencies to PYTHONPATH
 # sys.path.append("/home/dataman/Code/qualtrics_etl/src/qualtrics_etl/")
-source_dir = [os.path.join(os.path.dirname(os.path.abspath(__file__)), "../json_to_relation/")]
-source_dir.extend(sys.path)
-sys.path = source_dir
+# source_dir = [os.path.join(os.path.dirname(os.path.abspath(__file__)), "../json_to_relation/")]
+# source_dir.extend(sys.path)
+# sys.path = source_dir
 
 from surveyextractor import QualtricsExtractor
 
