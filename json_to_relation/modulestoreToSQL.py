84321606b53993caa6abb0f22e2af4919a6d4bc1
Alex Kindel <atkindel@stanford.edu>
2016-02-19 17:11:53 -0800

    Adjust EDXP field types. Parse split modulestore to relational format. Resolve timestamps.

--- json_to_relation/modulestoreToSQL.py
+++ json_to_relation/modulestoreToSQL.py
@@ -19,29 +19,23 @@
 ##  to a split modulestore (three Mongo databases). This class provides an interface to explicitly handle
 ##  either modulestore case.
 
+
 import sys
 import json
 import os.path
+import datetime
 from collections import defaultdict
 
 from pymysql_utils1 import MySQLDB
 import pymongo as mng
 
-class EdxProblemExtractor(MySQLDB):
 
-    ## TODO: Which problems matter?
-    # OLD_PROBLEM_CATEGORIES = [
-    #     'annotatable',
-    #     'combinedopenended',
-    #     'freetextresponse',
-    #     'imageannotation',
-    #     'peergrading',
-    #     'poll_question',
-    #     'problem',
-    #     'submit-and-compare',
-    #     'textannotation',
-    #     'videoannotation'
-    # ]
+class ObjectReprEncoder(json.JSONEncoder):
+    def default(self, obj):
+        return repr(obj)
+
+
+class EdxProblemExtractor(MySQLDB):
 
     def __init__(self, split=True, old=True):
         '''
@@ -49,13 +43,13 @@ class EdxProblemExtractor(MySQLDB):
         Note: This class presumes modulestore was recently loaded to mongod.
         Class also presumes that mongod is running on localhost:27017.
         '''
-        self.msdb = mng.MongoClient().modulestore ## XXX: am I reading all of MS into memory here?
+        self.msdb = mng.MongoClient().modulestore
 
         # Need to handle Split and Old modulestore cases
         self.split = split
         self.old = old
 
-        # Initialize MySQL connection
+        # Initialize MySQL connection from config file
         home = os.path.expanduser('~')
         dbFile = home + "/.ssh/mysql_user"
         if not os.path.isfile(dbFile):
@@ -70,6 +64,7 @@ class EdxProblemExtractor(MySQLDB):
 
     def __buildEmptyEdxProblemTable(self):
         '''
+        Reset EdxProblem table and rebuild.
         '''
         # Build table drop and table create queries
         dropOldTableQuery = """DROP TABLE IF EXISTS `EdxProblem`;"""
@@ -78,21 +73,14 @@ class EdxProblemExtractor(MySQLDB):
               `problem_id` VARCHAR(32) DEFAULT NULL,
               `problem_display_name` VARCHAR(100) DEFAULT NULL,
               `course_display_name` VARCHAR(100) DEFAULT NULL,
-              `problem_text` TEXT,
-              `date` DATETIME DEFAULT NULL,
+              `problem_text` LONGTEXT,
+              `date` VARCHAR(50) DEFAULT NULL,
               `weight` INT DEFAULT NULL,
               `revision` VARCHAR(10) DEFAULT NULL,
               `max_attempts` INT DEFAULT NULL,
               `trackevent_hook` VARCHAR(200) DEFAULT NULL
             ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
         """
-        ## TODO NOTE re: above table definition
-        # published_date from metadata or edit_info? either?
-        # revision is either 'draft' or null-- might help with 'was this a real question'
-        # problem_text can have a lot of data and potentially code
-        # get CDN from modulestoreImporter.py or just reproduce that here
-        # trackevent_hook makes easy joins with EdxTrackEvent
-        ####
 
         # Execute table definition queries
         self.execute(dropOldTableQuery)
@@ -102,26 +90,42 @@ class EdxProblemExtractor(MySQLDB):
     def export(self):
         '''
         Client method builds tables and loads various modulestore cases to MySQL.
+        We reload each time. The full load takes less than a minute.
         '''
         self.__buildEmptyEdxProblemTable()
-        if self.split: self.__extractSplitMS()
+        if self.split:
+            splitdata = self.__extractSplitMS()
+            self.__loadToSQL(splitdata)
         if self.old:
-            table = self.__extractOldMS()
-            self.__loadOldMS(table)
+            oldmsdata = self.__extractOldMS()
+            self.__loadToSQL(oldmsdata)
 
 
     @staticmethod
-    def __extractLogHook(problem):
+    def __resolveResourceURI(problem):
         '''
-        Extract resource identifier as identifier for EdxTrackEvent hook.
+        Extract resource URI as identifier for EdxTrackEvent hook.
         '''
         tag = problem["_id"]["tag"]
         org = problem["_id"]["org"]
         course = problem["_id"]["course"]
         category = problem["_id"]["category"]
         name = problem["_id"]["name"]
-        hook = "%s://%s/%s/%s/%s" % (tag, org, course, category, name)
-        return hook
+        uri = "%s://%s/%s/%s/%s" % (tag, org, course, category, name)
+        return uri
+
+
+    @staticmethod
+    def __resolveTimestamp(problem):
+        '''
+        Convert published_date array from modulestore to python datetime object.
+        '''
+        dtarr = problem['metadata'].get('published_date', False)
+        if not dtarr:
+            return None
+        dtstr = '-'.join(map(str, dtarr[:6]))
+        date = datetime.datetime.strptime(dtstr, "%Y-%m-%d-%H-%M-%S")
+        return date
 
 
     def __resolveCDN(self, problem):
@@ -130,11 +134,9 @@ class EdxProblemExtractor(MySQLDB):
         '''
         org = problem["_id"]["org"]
         course = problem["_id"]["course"]
-
         definition = self.msdb.modulestore.find({"_id.category": "course",
                                                  "_id.org": org,
                                                  "_id.course": course})
-
         name = definition[0]["_id"]["name"]
         cdn = "%s/%s/%s" % (org, course, name)
         return cdn
@@ -142,42 +144,81 @@ class EdxProblemExtractor(MySQLDB):
 
     def __extractOldMS(self):
         '''
+        Extract problem data from old-style MongoDB modulestore.
+        SQL load method expects a list of dicts mapping column names to data.
         '''
         problems = self.msdb.modulestore.find({"_id.category": "problem"})
         table = []
         for problem in problems:
-            data = defaultdict(list)
+            # Each row in the table is a dictionary
+            data = dict()
             data['problem_id'] = problem['_id'].get('name', 'False')
             data['problem_display_name'] = problem['metadata'].get('display_name', 'False')
             data['course_display_name'] = self.__resolveCDN(problem)
             data['problem_text'] = problem['definition'].get('data', 'False')
-            data['date'] = problem['metadata'].get('published_date', 'False')
+            data['date'] = self.__resolveTimestamp(problem)
             data['weight'] = problem['metadata'].get('weight', -1)
             data['revision'] = problem['_id'].get('revision', 'False')
             data['max_attempts'] = problem['metadata'].get('max_attempts', -1)
-            data['trackevent_hook'] = self.__extractLogHook(problem)
+            data['trackevent_hook'] = self.__resolveResourceURI(problem)
             table.append(data)
 
         return table
 
-    def __loadOldMS(self, table):
-        #columns = 'problem_id', 'problem_display_name', 'course_display_name', 'problem_text', 'date', 'weight', 'revision', 'max_attempts', 'trackevent_hook'
+
+    def __extractSplitMS(self):
+        '''
+        Extract problem data from Split MongoDB modulestore.
+        SQL load method expects a list of dicts mapping column names to data.
+        '''
+        table = []
+
+        # Get a course generator and iterate through
+        courses = self.msdb['modulestore.active_versions'].find()
+        for course in courses:
+            cdn = "%s/%s/%s" % (course['org'], course['course'], course['run'])
+            cid = course['versions'].get('published-branch', None)
+            if not cid:
+                continue
+
+            # Retrieve course structure from published branch and filter out non-problem blocks
+            structure = self.msdb['modulestore.structures'].find({"_id": cid, "blocks.block_type": "problem"})[0]
+            for block in filter(lambda b: b['block_type'] == 'problem', structure['blocks']):
+                definition = self.msdb['modulestore.definitions'].find({"_id": block['definition']})[0]
+
+                # Construct data dict and append to table list
+                data = dict()
+                data['problem_id'] = block['block_id']
+                data['problem_display_name'] = block['fields'].get('display_name', "NA")
+                data['course_display_name'] = cdn
+                data['problem_text'] = definition['fields']['data']
+
+                # TODO: Test the below on real course data from split modulestore
+                data['date'] = False
+                data['weight'] = -1
+                data['revision'] = False
+                data['max_attempts'] = -1
+                data['trackevent_hook'] = False
+                table.append(data)
+
+        return table
+
+
+    def __loadToSQL(self, table):
+        '''
+        Build columns tuple and list of row tuples for MySQLDB bulkInsert operation, then execute.
+        '''
         columns = table[0].keys()
 
         data = []
         for row in table:
-            values = tuple(row.values())
+            values = tuple(row.values())  # Convert each dict value set to tuple for loading
             data.append(values)
+
         self.bulkInsert('EdxProblem', columns, data)
-
-
-    def __extractSplitMS(self):
-        '''
-        '''
-        print "Split case empty."
 
 
 
 if __name__ == '__main__':
-    extractor = EdxProblemExtractor(split=False)
+    extractor = EdxProblemExtractor()
     extractor.export()
