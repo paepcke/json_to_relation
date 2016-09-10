367db8301871a487108e3fd261a4accccec2114a
Andreas Paepcke <paepcke@cs.stanford.edu>
2016-09-10 09:42:38 -0700

    Added procedure allHomeworkSumissions(course_display_name).

--- scripts/mysqlProcAndFuncBodies.sql
+++ scripts/mysqlProcAndFuncBodies.sql
@@ -1655,6 +1655,28 @@ FROM edxprod.auth_userprofile
  LEFT JOIN Edx.UserCountry
    ON Edx.UserCountry.anon_screen_name = EdxPrivate.UserGrade.anon_screen_name;
 
+#--------------------------
+# allHomeworkSubmissions
+#---------------------
+
+# Given a course_display_name, produce all homework submissions.
+# Note: for the most recent, use table ActivityGrade.
+
+DROP PROCEDURE IF EXISTS allHomeworkSubmissions//
+CREATE PROCEDURE allHomeworkSubmissions(IN the_course_display_name varchar(255))
+BEGIN
+    SELECT DISTINCT course_display_name,
+                    time,
+                    anon_screen_name,
+                    Answer.problem_id,
+                    answer
+      FROM EdxTrackEvent, Answer
+     WHERE EdxTrackEvent.answer_fk = Answer.answer_id
+       AND event_type LIKE '%problem_check%'
+       AND course_display_name = the_course_display_name
+       AND answer_fk != '';
+END//
+
 # ------------- Drop Functions that are only for EdxPrivate DB -----
 
 DROP FUNCTION IF EXISTS EdxPiazza.idForum2Anon;
@@ -1681,6 +1703,7 @@ CALL grantExecuteIfExists('Edx.enrollment');
 CALL grantExecuteIfExists('Edx.computeEnrollmentCoursera');
 CALL grantExecuteIfExists('Edx.computeEnrollmentNovoEd');
 CALL grantExecuteIfExists('Edx.multipleDbQuery');
+CALL grantExecuteIfExists('Edx.allHomeworkSubmissions');
 
 CALL grantExecuteIfExists('EdxPrivate.idInt2Anon');
 CALL grantExecuteIfExists('EdxPrivate.idAnon2Int');
@@ -1694,6 +1717,7 @@ CALL grantExecuteIfExists('EdxPrivate.enrollment');
 CALL grantExecuteIfExists('EdxPrivate.computeEnrollmentCoursera');
 CALL grantExecuteIfExists('EdxPrivate.computeEnrollmentNovoEd');
 CALL grantExecuteIfExists('EdxPrivate.multipleDbQuery');
+CALL grantExecuteIfExists('EdxPrivate.allHomeworkSubmissions');
 
 CALL grantExecuteIfExists('EdxForum.idInt2Anon');
 CALL grantExecuteIfExists('EdxForum.idAnon2Int');
@@ -1707,6 +1731,7 @@ CALL grantExecuteIfExists('EdxForum.enrollment');
 CALL grantExecuteIfExists('EdxForum.computeEnrollmentCoursera');
 CALL grantExecuteIfExists('EdxForum.computeEnrollmentNovoEd');
 CALL grantExecuteIfExists('EdxForum.multipleDbQuery');
+CALL grantExecuteIfExists('EdxForum.allHomeworkSubmissions');
 
 CALL grantExecuteIfExists('EdxPiazza.idInt2Anon');
 CALL grantExecuteIfExists('EdxPiazza.idAnon2Int');
@@ -1720,6 +1745,7 @@ CALL grantExecuteIfExists('EdxPiazza.enrollment');
 CALL grantExecuteIfExists('EdxPiazza.computeEnrollmentCoursera');
 CALL grantExecuteIfExists('EdxPiazza.computeEnrollmentNovoEd');
 CALL grantExecuteIfExists('EdxPiazza.multipleDbQuery');
+CALL grantExecuteIfExists('EdxPiazza.allHomeworkSubmissions');
 
 CALL grantExecuteIfExists('EdxQualtrics.idInt2Anon');
 CALL grantExecuteIfExists('EdxQualtrics.idAnon2Int');
@@ -1733,6 +1759,7 @@ CALL grantExecuteIfExists('EdxQualtrics.enrollment');
 CALL grantExecuteIfExists('EdxQualtrics.computeEnrollmentCoursera');
 CALL grantExecuteIfExists('EdxQualtrics.computeEnrollmentNovoEd');
 CALL grantExecuteIfExists('EdxQualtrics.multipleDbQuery');
+CALL grantExecuteIfExists('EdxQualtrics.allHomeworkSubmissions');
 
 CALL grantExecuteIfExists('unittest.idInt2Anon');
 CALL grantExecuteIfExists('unittest.idAnon2Int');
@@ -1746,3 +1773,4 @@ CALL grantExecuteIfExists('unittest.enrollment');
 CALL grantExecuteIfExists('unittest.computeEnrollmentCoursera');
 CALL grantExecuteIfExists('unittest.computeEnrollmentNovoEd');
 CALL grantExecuteIfExists('unittest.multipleDbQuery');
+CALL grantExecuteIfExists('unittest.allHomeworkSubmissions');
