38431c335c8d1426b4635316403045ca9150d311
Andreas Paepcke <paepcke@cs.stanford.edu>
2017-12-19 16:59:22 -0800

    Added ability to provide an SQL WHERE clause calling removeTestCourses.sh

--- scripts/removeTestCourses.sh
+++ scripts/removeTestCourses.sh
@@ -16,7 +16,11 @@
 # starting with zero, where the course_display_name (course_id, course name)
 # reside.
 #
+# If a select condition is provided, it must be a MySQL "where" clause.
+# It will be applied to the exported table.
+#
 #     Example: removeTestCourses.sh dataman edxprod.courseware_studentmodule 10 Misc.courseware_studentmodule_clean
+#     Example: removeTestCourses.sh dataman edxprod.courseware_studentmodule 10 Misc.courseware_studentmodule_clean "where the_date > '2010-09-03'"
 #
 # Strategy:
 #
@@ -33,12 +37,42 @@
 #          it knows naught of text columns that contain commas.
 #          Use only for tables that have no commas in their columns.
 #          To fix this shortcoming: edit removeTestCourses.awk.
+
+# Possible Test table:
+# 
+#  SELECT * FROM VideoInteraction;
+#  
+#  -->+----------+-------------------------------------+------------+------------------+------------+
+#     | video_id | course_display_name                 | quarter    | anon_screen_name | date       |
+#     +----------+-------------------------------------+------------+------------------+------------+
+#     | v1       | engineering/sefu/winter2015         | spring2015 | s0               | NULL       |
+#     | v1       | internal/101/private_testing_course | winter2016 | s1               | 2014-01-01 |
+#     | v2       | humanities/phil202/spring2015       | spring2015 | s1               | 2015-01-01 |
+#     | v2       | humanities/phil202/spring2015       | winter2016 | s1               | 2015-01-01 |
+#     | v2       | engineering/cs101/winter2016        | winter2016 | s2               | 2016-01-01 |
+#     +----------+-------------------------------------+------------+------------------+------------+
+#     5 rows in set (0.01 sec)
+#
+#  removeTestCourses.sh paepcke Trash.VideoInteraction 1 Trash.VideoInteraction_clean "where year(date) < 2016"
+#
+#  Should yield in db:
+#
+# SELECT * FROM Trash.VideoInteraction_clean;
+#
+#     +----------+-------------------------------+------------+------------------+------------+
+#     | video_id | course_display_name           | quarter    | anon_screen_name | date       |
+#     +----------+-------------------------------+------------+------------------+------------+
+#     | v2       | humanities/phil202/spring2015 | spring2015 | s1               | 2015-01-01 |
+#     | v2       | humanities/phil202/spring2015 | winter2016 | s1               | 2015-01-01 |
+#     +----------+-------------------------------+------------+------------------+------------+
+#
+# 
 #
 #---------------------------------------------------------
 
-USAGE="USAGE: $(basename $0) <mysql_user> <full_src_tbl_name> <crse_name_index> <full_new_table_name>"
+USAGE="USAGE: $(basename $0) <mysql_user> <full_src_tbl_name> <crse_name_index> <full_new_table_name> [<select_condition>]"
 
-if [[ $# != 4 ]]
+if [[ $# < 4 ]]
 then
     echo $USAGE
     exit 1
@@ -48,6 +82,22 @@ MYSQL_USER=$1
 SRC_TBL_NAME=$2
 CRSE_NAME_INDEX=$3
 NEW_TBL_NAME=$4
+
+# Was a select condition provided on the CLI?
+
+SELECT_CONDITION=''
+if [[ $# == 5 ]]
+then
+    SELECT_CONDITION=$5
+fi    
+
+#****************
+# echo "MYSQL_USER      : ${MYSQL_USER}"
+# echo "SRC_TBL_NAME    : ${SRC_TBL_NAME}"
+# echo "CRSE_NAME_INDEX : ${CRSE_NAME_INDEX}"
+# echo "NEW_TBL_NAME    : ${NEW_TBL_NAME}"
+# echo "SELECT_CONDITION: ${SELECT_CONDITION}"
+#****************
 
 # Ensure that src and dest have database names:
 # such as MyDb.myTable:
@@ -98,9 +148,15 @@ trap cleanup EXIT
 
 read -rd '' TBL_EXPORT_CMD <<EOF
 SELECT * FROM ${SRC_TBL_NAME}
+  ${SELECT_CONDITION}
   INTO OUTFILE '${TMP_FILE}'
-  FIELDS TERMINATED BY "," OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
+  FIELDS TERMINATED BY "," OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n';
+  
 EOF
+
+#***********
+#echo "TBL_EXPORT_CMD: ${TBL_EXPORT_CMD}"
+#***********
 
 mysql --login-path=${MYSQL_USER} -e "$TBL_EXPORT_CMD"
 
